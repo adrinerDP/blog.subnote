64f92a9a4168667dc16d9d336b1782589c7b41f5
adrinerDP <me@adrinerdp.co>
2020-09-09 20:45:59 +0900

    feat: remove search function + js perf

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,19 +70,5 @@
                 v.play();
             }).show();
         }
-
-        // turn img alt into caption
-        $('#post-content > p > img[alt]').replaceWith(function () {
-            return '<figure>'
-                + '<a href="' + $(this).attr('src') + '" class="mg-link">'
-                + '<img src="' + $(this).attr('src') + '"/></a>'
-                + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
-                + '</figure>';
-        });
-        // and connect magnific popup image viewer
-        $('#post-content .mg-link').magnificPopup({
-            type: 'image',
-            closeOnContentClick: true
-        });
     });
 }(window, window.document, window.jQuery));
